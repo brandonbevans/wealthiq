556180e04847b5a737d52c01e95f5df497226aa9
Brandon <brandonbevans@gmail.com>
2025-11-10 08:34:29 -0500

    Initial Commit

---
+++ wealthiq/wealthiqApp.swift
@@ -0,0 +1,17 @@
+//
+//  wealthiqApp.swift
+//  wealthiq
+//
+//  Created by Brandon Bevans on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct wealthiqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
